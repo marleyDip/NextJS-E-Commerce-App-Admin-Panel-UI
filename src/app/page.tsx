eef8f0f2ce7c0eec446f0c3b9066f1a483084ee0
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-09-26 00:27:06 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 const Homepage = () => {
-  return (
-    <div className='bg-red-200'>Homepage</div>
-  )
-}
+  return <div className="">Homepage</div>;
+};
 
-export default Homepage
+export default Homepage;
